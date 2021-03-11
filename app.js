f4e29f44b6247b207424c6937b684f2cbbb1bad5
bagwanbulbul <bagwanbulbul>
2021-03-11 16:02:54 +0530

    created APIs for search data

--- app.js
+++ app.js
@@ -83,7 +83,7 @@ app.get("/sortedByJoiningDate",(req,res)=>{
     })
 })
 app.get("/sortedByBirthDate",(req,res)=>{
-    Employee.find().sort({JoiningDate: -1})
+    Employee.find().sort({BirthDate: -1})
     .then((data)=>{
         console.log(data)
         res.send(data)
@@ -116,22 +116,94 @@ app.post("/updateData",(req,res)=>{
         Pincode:Pincode}})
     .then(result =>{
         res.send({data:"Record has been Updated..!!"});  
-        //res.json({statusCode:"200",statusMsj:"Successfuly Update", data:result})
     }).catch(err =>{
         res.send(err)
     })
 })
-
 app.post("/deleteData",function(req,res){   
     Employee.remove({ _id: req.body.user_id }, function(err) {  
-            if(err){  
-                res.send(err);  
+        if(err){  
+            res.send(err);  
+        }  
+        else{    
+                res.send({data:"Record has been Deleted..!!"});             
             }  
-            else{    
-                    res.send({data:"Record has been Deleted..!!"});             
-                }  
-        });  
-   })  
+    });  
+})
+
+app.get("/searchName",function(req,res){
+    const name = req.body.name;
+    Employee.find({'name': {$regex:name, $options:'i'}})
+    .then(result=>{
+        if(result.length == 0){
+            res.send("Name Not Found !!")
+        }
+        res.send(result)
+    }).catch(err=>{
+        console.log(err)
+        res.send(err)
+    })
+})
+app.get("/EmployiesFromIndore",function(req,res){
+    Employee.find().then((data)=>{
+        var result = []
+        for(var i in data){
+            if(data[i].Address === "Indore"){
+                result.push(data[i])
+            }
+        } 
+        res.send(result)
+    }).catch(err=>{
+        console.log(err)
+        res.send(err)
+    })
+})
+
+app.get("/salaryByRange",function(req,res){
+    var num1 = req.body.num1;
+    var num2 = req.body.num2;
+    Employee.find({Salary:{$gt :num1, $lt : num2}})
+    .then(data=>{
+        res.send(data)
+    }).catch(err=>{
+        console.log(err)
+        res.send(err)
+    })
+})
+
+app.get("/neededBuses",function(req,res){
+    Employee.find().then(data=>{
+        var count = 1
+        var j = 1
+        for(var i=0;  i<=data.length; i++){
+            const d1 = data[i].Pincode
+            const d2 = data[j].Pincode
+            if(d1 != d2){
+                count = count+1
+            }
+            j++
+            res.json({Buss:count})
+        }
+    }).catch(err=>{
+        console.log(err)
+        res.send("SOMETHING WENT WRONG")
+    })
+})
+
+app.get("/TotalEmployiesSalary",function(req,res){
+    Employee.find().then(data=>{
+        console.log(data)
+        var sum = 0
+        data.forEach(item=>{
+            const employeeSalary = item.Salary
+            sum = sum+employeeSalary
+        })
+        res.json({ToatalSalary: sum})
+    }).catch(err=>{
+        console.log(err)
+        res.send(err)
+    })
+})
 app.listen(PORT, () => {
     console.log('Server is listening on Port:', PORT)
 })
